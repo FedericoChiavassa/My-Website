b0f4f2f40671f9e98d9721128729851baba2fec8
Fede <federico.chiavassa@hotmail.it>
2019-08-03 16:19:07 +0200

    improvements and responsiveness

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,3 +2,17 @@
 const scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 600
 });
+
+// Menu-icon Animation
+function iconAnimation(x) {
+    const width = window.innerWidth;
+    if ( width < 684 ) {
+        document.getElementById("menu-icon").classList.toggle("change");
+        document.getElementById("navbar").classList.toggle("change");
+    } else {
+        if ( x.nodeName === "DIV" ) {
+            document.getElementById("menu-icon").classList.toggle("change");
+            document.getElementById("navbar").classList.toggle("change");
+        }
+    }
+}
